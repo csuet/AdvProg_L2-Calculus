d92aca78557e2a08c7cbc09b48155278dbcc6254
TDungx <100231374+TDungx@users.noreply.github.com>
2022-03-03 23:19:49 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <math.h>
 
 using std::string;
 using std::cout;
@@ -20,7 +21,7 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,7 +32,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
 
 
@@ -46,7 +47,9 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    else
+    {
+        return sqrt(x)
+    }
     
-    return 0;
 }
