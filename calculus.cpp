40828f40dcf50b691465ce6df342c312136fffa5
ngobinhduong <21020756@vnu.edu.vn>
2022-03-03 23:43:42 +0700

    NgoBinhDuong_21020756

--- calculus.cpp
+++ calculus.cpp
@@ -7,7 +7,6 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
@@ -48,8 +47,6 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-
     return 0;
     return sqrt(x);
 } 
