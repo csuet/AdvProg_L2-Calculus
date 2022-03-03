8f653e79891004e76499431d2e0365a483afc9c6
Quang Ngoc <21020550@vnu.edu.vn>
2022-03-03 18:59:32 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -12,6 +12,17 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
+double giaiThua(int x) {
+    int kq =1;
+    if (x == 0) {
+        return kq;
+    }
+    for (int i = 1; i <=x;i++) {
+        kq *= i;
+    }
+    return kq;
+}
+
 /***
     Args:
         x (double): a number
@@ -20,7 +31,15 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    double i = 0;
+    double cos = 0;
+    double oldcos = -1;
+    do {
+        oldcos = cos;
+        cos += (pow(-1,i)*pow(x,2*i))/giaiThua(2*i);
+    } while (cos - oldcos > 0.001);
+    
+    return cos;
 }
 
 /***
@@ -31,7 +50,15 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double i = 0;
+    double sin = 0;
+    double oldsin = -1;
+    do {
+        oldsin = sin;
+        sin += (pow(-1,i)*pow(x,2*i+1))/giaiThua(2*i+1);
+    } while (sin - oldsin > 0.001);
+    
+    return sin;
 }
 
 
@@ -46,7 +73,17 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
+    if (x == 1) {
+        return 1;
+    }
+    double sqrt = 0;
+    double oldsqrt = -1;
+    double x0 = 10.5;
+    do {
+        oldsqrt = sqrt;
+        sqrt += x0 - (pow(x0,2)-x)/2*x0;
+        x0 = sqrt;
+    } while (sqrt - oldsqrt > 0.0001);
 
-    
-    return 0;
+    return sqrt;
 }
