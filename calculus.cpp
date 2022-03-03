fc8a91b1d9092de4d5f23ba219780f566f577e20
godjulius <duongnp28072003@gmail.com>
2022-03-04 02:00:42 +0700

    submit

--- calculus.cpp
+++ calculus.cpp
@@ -20,7 +20,7 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,10 +31,9 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
-
-
+//aaaaa
 /***
     Args:
         x (double): a number
@@ -46,7 +45,6 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
     
-    return 0;
+    return sqrt(x);
 }
