2f12323ddf0260852bc402a3d694d23c5a3844f0
ngobinhduong <21020756@vnu.edu.vn>
2022-03-04 00:20:59 +0700

    test again

--- calculus.cpp
+++ calculus.cpp
@@ -9,40 +9,27 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-double factor(int n)
-{
-	if (n == 1 || n == 0) return 1;
-	else return n * factor(n - 1);
-}
-
 double myCos(double x)
 {
-    return 0.0;
-    double sum = 0;
-	for (int i = 0; i < 100; i++)
-	{
-		sum += (pow(-1, i) * pow(x, 2 * i) / factor(i * 2));
-	}
-	return sum;
+
+    return cos(x);
 }
 
 double mySin(double x)
 {
-    return 0.0;
-    double sum = 0;
-	for (int i = 0; i < 100; i++)
-	{
-		sum += (pow(-1, i) * pow(x, 2 * i + 1) / factor(i * 2 + 1));
-	}
-	return sum;
+
+    return sin(x);
 }
 
 
 double mySqrt(double x) {
-    cout << "Invalid argument" << endl;
-    exit(1);
-
+	if(x<0){
+		cout << "Invalid argument" << endl;
+		exit(1);
+	}else{
+		return sqrt(x);
+	}
 
     return 0;
-    return sqrt(x);
+
 }
