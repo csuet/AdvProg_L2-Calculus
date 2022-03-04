c3a01b0042bed70f303d3ff03dbd845fb101592d
Maheu1010 <100405724+Maheu1010@users.noreply.github.com>
2022-03-04 18:00:01 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -47,8 +47,11 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
+    }
     }else{
-     return sqrt (x);}
+     return sqrt (x);
+    }
     return 0;
        
+}
 }
