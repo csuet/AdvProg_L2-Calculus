674ad7a871dcdca51ddafa35f5e1afd1f8f1cd38
TranManhDung21020119 <21020119@vnu.edu.vn>
2022-03-04 22:05:48 +0700

    21020119 Tran Manh Dung

--- calculus.cpp
+++ calculus.cpp
@@ -27,8 +27,8 @@ double factorial(long long n){
 double myCos(double x) 
 {
     double cosin = 0;
-    for (long long i = 0; i < 100; i++){
-        cosin += pow(x, i) * pow(-1, i / 2) / factorial(i)
+    for (long long i = 0; i < 200; i += 2){
+        cosin += (pow(x, i) * pow(-1, i / 2)) / factorial(i);
     }
     return cosin;
 }
@@ -42,8 +42,8 @@ double myCos(double x)
 double mySin(double x)
 {
     double sin = 0;
-    for (long long i = 0; i < 100; i++){
-        sin += pow(x, i) * pow(-1, (i - 1) / 2) / factorial(i);
+    for (long long i = 1; i < 200; i += 2){
+        sin += (pow(x, i) * pow(-1, (i - 1) / 2)) / factorial(i);
     }
     return sin;
 }
