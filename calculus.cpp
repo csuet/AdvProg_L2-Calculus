6dbe9b61100642cf86619f921fcb6ffcb1db2c5c
Maheu1010 <100405724+Maheu1010@users.noreply.github.com>
2022-03-04 17:05:26 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -21,6 +21,7 @@ double mySqrt(double x);
 double myCos(double x) 
 {
     return 0.0;
+    return cos(x);
 }
 
 /***
@@ -32,6 +33,7 @@ double myCos(double x)
 double mySin(double x)
 {
     return 0.0;
+    return sin(x);
 }
 
 
@@ -47,6 +49,6 @@ double mySqrt(double x) {
         exit(1);
     }
 
-    
     return 0;
+    return sqrt(x);
 }
