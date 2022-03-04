48b23c667f58c11aec56fed2a3139ae04cf7b77c
Reiner-Braun-111 <90023350+Reiner-Braun-111@users.noreply.github.com>
2022-03-04 17:52:38 +0700

    p

--- calculus.cpp
+++ calculus.cpp
@@ -5,7 +5,7 @@
 using std::string;
 using std::cout;
 using std::endl;
-using std::stod;
+//using std::stod;
 
 
 double mySin(double x);
@@ -20,7 +20,7 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,7 +31,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
 
 
@@ -45,8 +45,10 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
-    }
+    }else{
+    	return sqrt(x);
+	}
 
     
-    return 0;
+    
 }
