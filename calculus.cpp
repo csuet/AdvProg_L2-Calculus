2bd59f6c795065b411fabf0a76a1e924e865ac06
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-03-05 03:08:01 +0700

    khai triển taylor

--- calculus.cpp
+++ calculus.cpp
@@ -19,8 +19,24 @@ double mySqrt(double x);
         double: cosine of x
 ***/
 double myCos(double x) 
-{
-    return cos(x);
+{   
+    
+    const double PI_TEST = 3.14159265359;
+    while(x>PI_TEST){
+        x-=2*PI_TEST;
+    }
+    double sum=1, old_sum=0;
+    long long factorial=1;
+    double exponential=1;
+    int i=1;
+    while(abs(sum-old_sum)>0.0001){
+        old_sum=sum;
+        exponential=exponential*(x*x);
+        factorial=factorial*(2*i)*(2*i-1);
+        sum+=(pow(-1,i)/(factorial))*exponential;
+        i++;
+    }
+    return sum;
 }
 
 /***
@@ -30,8 +46,23 @@ double myCos(double x)
         double: sine of x
 ***/
 double mySin(double x)
-{
-    return sin(x);
+{   
+    const double PI_TEST = 3.14159265359;
+    while(x>PI_TEST){
+        x-=2*PI_TEST;
+    }
+    double sum=x, old_sum=0;
+    long long factorial=1;
+    double exponential=x;
+    int i=1;
+    while(abs(sum-old_sum)>0.0001){
+        old_sum=sum;
+        exponential=exponential*(x*x);
+        factorial=factorial*(2*i)*(2*i+1);
+        sum+=(pow(-1,i)/(factorial))*exponential;
+        i++;
+    }
+    return sum;
 }
 
 
