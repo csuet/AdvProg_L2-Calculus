0e85c4ee1de1330edd306454edc2900c3d6b22d9
John Doe <21020757@vnu.edu.vn>
2022-03-04 21:25:22 +0700

    21020757_Nguyễn Đăng Dương

--- calculus.cpp
+++ calculus.cpp
@@ -20,7 +20,22 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return cos(x);
+    double sum = x;
+    int i = 2;
+    double k = x*x*(-1)/i;
+    sum += k;
+    i++;
+    do
+    {
+        k = k*x/i;
+        if (i % 2 == 0) {
+            k = k *(-1);
+            sum += k;
+        }
+        i++;
+    }
+    while (fabs(k) > 0.001);
+    return sum;
 }
 
 /***
@@ -31,7 +46,23 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double sum = x;
+    int i = 3;
+    double k = x*x*x*(-1)/i;
+    k/=2;
+    sum += k;
+    i++;
+    do
+    {
+        k = k*x/i;
+        if (i % 2 != 0) {
+            k = k *(-1);
+            sum += k;
+        }
+        i++;
+    }
+    while (fabs(k) > 0.001);
+    return sum;
 }
 
 
@@ -42,11 +73,18 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
+    double sum = 10;
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return sqrt(x);
+    else {
+        double k = double(sum * sum - x) / (2 * sum);
+        sum -= k;
+        do {
+            k = double(sum * sum - x) / (2 * sum);
+            sum -= k;
+        } while (fabs(k) > 0.001);
+    }
+    return sum;
 }
