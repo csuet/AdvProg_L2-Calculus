d608c5fe167ee181ea2c7c2b78ef08d727c9e26c
Le_Cong_Hoang <21020322@vnu.edu.vn>
2022-03-04 16:40:57 +0700

    21020322 test commit

--- calculus.cpp
+++ calculus.cpp
@@ -51,7 +51,7 @@ double mySin(double x)
     int k=1; int pos = 1;
     while (abs()>0.00001)
     {
-        result+=((pos%2)!=0?( (1/factorial(k))*pow(x,k) : (-(1/factorial(k))*pow(x,k)));
+        result+=((pos%2)!=0?( ((1/factorial(k))*pow(x,k)) : (-(1/factorial(k))*pow(x,k))));
         k+=2; pos++;
     }
     return result;
