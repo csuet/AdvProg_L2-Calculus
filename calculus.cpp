e1648c2777b07aef577adc721372c9ab857aa677
Khanhhhhhhhh <21020343@vnu.edu.vn>
2022-03-06 01:14:29 +0700

    21020343 - Trinh Van Khanh

--- calculus.cpp
+++ calculus.cpp
@@ -11,7 +11,13 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+double factorial(double n);
 
+double factorial(double n)
+{
+    if (n == 1) return 1;
+    else return n*factorial(n-1);
+}
 /***
     Args:
         x (double): a number
@@ -20,7 +26,15 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return cos(x);
+    int n;
+    double cosx = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % 2 == 0) n = 1;
+        else n = -1;
+        cosx += n*pow(x,2*i)/factorial(2*i);
+    }
+    return cosx;
 }
 
 /***
@@ -31,7 +45,14 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    int n, sinx = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (i % 2 == 0) n = 1;
+        else n = -1;
+        sinx += n*pow(x,2*n+1)/factorial(2*n+1);
+    }
+    return sinx;
 }
 
 
@@ -48,6 +69,13 @@ double mySqrt(double x)
         cout << "Invalid argument" << endl;
         exit(1);
     }
-    else return sqrt(x);
+    else
+    {
+        double error = 0.0000001;
+        double result = 1.0;
+        while (abs(result * result - x) / x >= error)
+        result = (x / result  - result) / 2 + result;
+        return result;
+    }
     return 0;
 }
