64b4997ba465ad1774caf0781d76c563ad93dea8
Uyen0408 <21020419@vnu.edu.vn>
2022-03-05 21:12:22 +0700

    21020419 Pham Tu Uyen

--- calculus.cpp
+++ calculus.cpp
@@ -20,7 +20,20 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return cos(x);
+    	double cosx=1;
+	int n=1;	
+	double temp=1;
+	while(temp>0.0001 || temp<-0.0001){
+		temp =1;
+		for(int i=1;i<=2*n;i++){
+			temp*=(x/(i));
+		}
+		if(n%2==0) cosx+=temp;
+		else cosx-=temp;
+		n++;
+	}
+	return cosx;
+
 }
 
 /***
@@ -31,7 +44,19 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+   	double sinx=x;
+	int n=1;	
+	double temp=1;
+	while(temp>0.0001 || temp<-0.0001){
+		temp =1;
+		for(int i=1;i<=2*n+1;i++){
+			temp*=(x/(i));
+		}
+		if(n%2==0) sinx+=temp;
+		else sinx-=temp;
+		n++;
+	}
+	return sinx;
 }
 
 
@@ -46,7 +71,22 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    double n;
+	for(int i=0;;i++){
+		n=pow(10,2*i);
+		if(x/n<1){
+			n=pow(10,i-1);
+			break;
+		}
+	}
+	double x0,x1=0;
+	if(x/n<10) x0=2*n;
+	else x0=6*n;
+	while(true){
+		x1=0.5*(x0+x/x0);
+		if(x1==x0) break;
+		x0=x1;
+	}
     
-    return sqrt(x);
+    return x0;
 }
