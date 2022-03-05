b5af7b4a6d42a82e4c5e6cdccae70e0cf6ba1cd0
hongquant17 <21020555@vnu.edu.vn>
2022-03-05 17:04:33 +0700

    21020555_Trần Hồng Quân

--- calculus.cpp
+++ calculus.cpp
@@ -12,6 +12,11 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
+double factorial(int n) {
+    if (n < 2) return 1;
+    return n * factorial(n - 1);
+}
+
 /***
     Args:
         x (double): a number
@@ -20,7 +25,11 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    double ans = 0;
+    for (int i = 0; i < 100; i++) {
+        ans += pow(-1, i) * pow (x, i * 2) * 1.0 / factorial(i * 2);
+    }
+    return ans;
 }
 
 /***
@@ -31,7 +40,11 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    double ans = 0;
+    for (int i = 0; i < 100; i++) {
+        ans += pow(-1, i) * pow (x, i * 2 + 1) * 1.0 / factorial(i * 2 + 1);
+    }
+    return ans;
 }
 
 
@@ -46,7 +59,7 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    else if (x > 0) return pow(x, 0.5);
     
     return 0;
 }
