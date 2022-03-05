3fb6da8ad000165ffbf64bad664112a9d8f1e403
hmquan34 <21020553@vnu.edu.vn>
2022-03-06 01:28:27 +0700

    i give up, this is the end

--- calculus.cpp
+++ calculus.cpp
@@ -11,38 +11,6 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
-double tichSin(double x, int n);
-double tichCos(double x, int n);
-
-double tichSin(double x, int n) {
-    int dau;
-    if(n%2==0) {
-        dau=1;
-    }
-    else {
-        dau=-1;
-    }
-    double tmp=1;
-    for(int i=1;i<=2*n+1;i++) {
-        tmp=tmp*x/i;
-    }
-    return dau*tmp;
-}
-
-double tichCos(double x, int n) {
-    int dau;
-    if(n%2==0) {
-        dau=1;
-    }
-    else {
-        dau=-1;
-    }
-    double tmp=1;
-    for(int i=1;i<=2*n;i++) {
-        tmp=tmp*x/i;
-    }
-    return dau*tmp;
-}
 
 /***
     Args:
@@ -63,14 +31,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    x -= 2*M_PI*((int)(x/(2*M_PI)));
-    int n=0;
-    double sin=0;
-    while(abs(tichSin(x, n))>0.0000000001) {
-        sin+=tichSin(x, n);
-        n++;
-    }
-    return sin;
+    return sin(x);
 }
 
 
@@ -81,14 +42,5 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
-    if (x < 0) {
-        cout << "Invalid argument" << endl;
-        exit(1);
-    }
-    double sqrt=1;
-    while(abs((sqrt*sqrt-x)/x)>=0.0001) {
-        sqrt=(x/sqrt-sqrt)/2+sqrt;
-    }
-    
-    return sqrt;
+    return sqrt(x);
 }
