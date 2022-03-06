e8a3941f0c489b21469c7101978dbecf14792cf7
PhamH1eu <21020319@vnu.edu.vn>
2022-03-06 23:46:40 +0700

    ex

--- calculus.cpp
+++ calculus.cpp
@@ -11,34 +11,18 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
-long long factorial(int x);
 
-long long factorial(int x){
-    long long ans=1;
-    for (int i=1;i<=x;i++) ans=ans*i;
-    return ans;
-}
 
 double myCos(double x) 
 {
-     double cos=0,precos; int m=0;
-     do{
-        precos=cos;
-        cos+=pow(-1,m)*pow(x,2*m)/factorial(2*m);
-        m++;
-     } while (abs(precos-cos)>=0.0000001);
-     return cos;
+     
+     return cos(x);
 }
 
 double mySin(double x)
 {
-    double sin=0,presin; int m=0;
-    do{
-        presin=sin;
-        sin+=pow(-1,m)*pow(x,2*m+1)/factorial(2*m+1);
-        m++;
-    } while (abs(presin-sin)>=0.0000001);
-    return sin;
+    
+    return sin(x);
 }
 
 double mySqrt(double x) {
@@ -46,10 +30,6 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-    double sqrt=x/2,preSqrt; 
-    do{
-        preSqrt=sqrt;
-        sqrt=(sqrt+x/sqrt)/2;
-    } while (abs(preSqrt-sqrt)>=0.000000001);
-    return sqrt;
+    
+    return sqrt(x);
 }
