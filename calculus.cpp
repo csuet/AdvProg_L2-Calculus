c346a8b9252855de242bdea32edfa54b7ddaa4a5
Nguyen Thi Quynh Mai <21020125@vnu.edu.vn>
2022-03-06 10:56:44 +0700

    test2

--- calculus.cpp
+++ calculus.cpp
@@ -11,14 +11,6 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
-int factorial(int x);
-
-const double e = 0.0001;
-
-int factorial(int x){
-    if (x == 0 || x == 1) return 1;
-    else return x*factorial(x-1);
-}
 
 /***
     Args:
@@ -28,18 +20,7 @@ int factorial(int x){
 ***/
 double myCos(double x) 
 {
-    double E = 1, cosx = 0;
-    int n = 0;
-    while(E > e){
-        E = pow(x, 2*n) / factorial(2*n);
-        if(n % 2 == 0){
-            cosx += E;
-        }else{
-            cosx -= E;
-        }
-        n++;
-    }
-    return cosx;
+    return cos(x);
 }
 
 /***
@@ -50,17 +31,7 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    double sinx = 0, E = 1;
-    int n = 0;
-    while(E > e){
-        E = pow(x, 2*n+1) / factorial(2*n+1);
-        if(n % 2 == 0){
-            sinx += E;
-        }else{
-            sinx -= E;
-        }
-    }
-    return sinx;
+    return sin(x);
 }
 
 
@@ -76,13 +47,6 @@ double mySqrt(double x) {
         exit(1);
     }
     else{
-        double result = 1;
-        if (x == 0) result = 0;
-        else{
-        	while(fabs(result*result - x) / x > e){
-            result = (x/result - result)/2 + result;
-            }
-		}
-        return result;
+        return sqrt(x);
     }
 }
