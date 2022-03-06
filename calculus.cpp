758e35aaf158b393e22e18f8b5f2928e3a592b27
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-03-06 16:01:34 +0700

    3 funtion

--- calculus.cpp
+++ calculus.cpp
@@ -22,24 +22,24 @@ double myCos(double x)
 {   
     
     
-    // while(x>PI){ 
-    //     x-=2*PI;
-    // }
-    // while(x<-PI){
-    //     x+=2*PI;
-    // }
-    // double sum=1, old_sum=0;
-    // long long factorial=1;
-    // double exponential=1;
-    // int i=1;
-    // while(abs(sum-old_sum)>0.0001){
-    //     old_sum=sum;
-    //     exponential=exponential*(x*x);
-    //     factorial=factorial*(2*i)*(2*i-1);
-    //     sum+=(pow(-1,i)/(factorial))*exponential;
-    //     i++;
-    // }
-    return cos(x);
+    while(x>PI){ 
+        x-=2*PI;
+    }
+    while(x<-PI){
+        x+=2*PI;
+    }
+    double sum=1, old_sum=0;
+    long long factorial=1;
+    double exponential=1;
+    int i=1;
+    while(abs(sum-old_sum)>0.0001){
+        old_sum=sum;
+        exponential=exponential*(x*x);
+        factorial=factorial*(2*i)*(2*i-1);
+        sum+=(pow(-1,i)/(factorial))*exponential;
+        i++;
+    }
+    return sum;
 }
 
 /***
@@ -48,27 +48,28 @@ double myCos(double x)
     Returns:
         double: sine of x
 ***/
+    
 double mySin(double x)
 {   
-    // const double PI = 3.14159265359;
-    // while(x>PI){
-    //     x-=2*PI;
-    // }
-    // while(x<-PI){
-    //     x+=2*PI;
-    // }
-    // double sum=x, old_sum=0;
-    // long long factorial=1;
-    // double exponential=x;
-    // int i=1;
-    // while(abs(sum-old_sum)>0.0001){
-    //     old_sum=sum;
-    //     exponential=exponential*(x*x);
-    //     factorial=factorial*(2*i)*(2*i+1);
-    //     sum+=(pow(-1,i)/(factorial))*exponential;
-    //     i++;
-    // }
-    return sin(x);
+    const double PI = 3.14159265359;
+    while(x>PI){
+        x-=2*PI;
+    }
+    while(x<-PI){
+        x+=2*PI;
+    }
+    double sum=x, old_sum=0;
+    long long factorial=1;
+    double exponential=x;
+    int i=1;
+    while(abs(sum-old_sum)>0.0001){
+        old_sum=sum;
+        exponential=exponential*(x*x);
+        factorial=factorial*(2*i)*(2*i+1);
+        sum+=(pow(-1,i)/(factorial))*exponential;
+        i++;
+    }
+    return sum;
 }
 
 
@@ -79,11 +80,16 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
+    double sum=10, old_sum=0;
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return sqrt(x);
+    else {
+        while(abs(sum-old_sum)>0.0001){
+        old_sum=sum;
+        sum=(sum+x/sum)/2;
+    }
+    return sum;
+}
 }
