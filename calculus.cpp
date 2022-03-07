bcc356bb3e46d8cf49ca33fe20bf4673c888a071
tung1883 <100570223+tung1883@users.noreply.github.com>
2022-03-08 03:15:35 +0700

    21020556 - Nguyen Duc Tung

--- calculus.cpp
+++ calculus.cpp
@@ -14,9 +14,8 @@ double mySqrt(double x);
 double cosineElement[1000]; // Array contains elements from Maclaurin formula for cosine //
 double sineElement[1000]; // Array contains elements from Maclaurin formula for sine //
 
-
-//Function to calculate the abstract value of the n-element from the Maclaurin expansion of cosine
-double absCosMaclaurin(double x, int n){ 
+//Function to calculate the n-element from the Maclaurin expansion of cosine
+double CosMaclaurin(double x, int n){ 
     if (n==0){
         cosineElement[0] = 1;
         return 1;
@@ -26,22 +25,10 @@ double absCosMaclaurin(double x, int n){
             return cosineElement[n];
         } 
         else {
-            cosineElement[n] = absCosMaclaurin(x,n-1)*x*x/(2*n*(2*n-1));
+            cosineElement[n] = -CosMaclaurin(x,n-1)*x*x/(2*n*(2*n-1));
             return cosineElement[n];
         }
     }
-}
-
-//Function to calculate the sign of n-element from the Maclaurin expansion of cosine 
-double cosineSign(int n){
-    double sign;
-    if (n%2==0){
-        sign = 1;
-    }
-    else {
-        sign = -1;
-    }
-    return sign;
 }
 
 /***
@@ -50,13 +37,11 @@ double cosineSign(int n){
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
-{
+double myCos(double x){
     double cos = 0;
     for (int i=0; i<1000; i++){
-        if (absCosMaclaurin(x,i)>1.7e-308){
-            double cosSign = cosineSign(i); 
-            cos += cosSign*absCosMaclaurin(x,i);
+        if (abs(CosMaclaurin(x,i))>0.001){
+            cos += CosMaclaurin(x,i);
         }
         else {
             break;
@@ -65,22 +50,17 @@ double myCos(double x)
     return cos;
 }
 
-//Function to calculate the abstract value of the n-element from the Maclaurin expansion of cosine
-double absSineMaclaurin(double x, int n){
+//Function to calculate the n-element from the Maclaurin expansion of cosine
+double SinMaclaurin(double x, int n){ 
     if (n==0){
         sineElement[0] = x;
-        return 1;
+        return x;
     }
     else {
-        if (sineElement[n]){
-            return sineElement[n];
-        } 
-        else {
-            sineElement[n] = absCosMaclaurin(x,n-1)*x*x*x/(2*n*(2*n-1)*(2*n+1));
-            return sineElement[n];
-        }
+        sineElement[n] = x*CosMaclaurin(x,n)/(2*n+1);
+        return sineElement[n];
     }
-} 
+}
 
 
 /***
@@ -93,9 +73,8 @@ double absSineMaclaurin(double x, int n){
 double mySin(double x){
     double sin = 0;
     for (int i=0; i<1000; i++){
-        if (absCosMaclaurin(x,i)>1.7e-308){
-            double sign = cosineSign(i);
-            sin += sign*absCosMaclaurin(x,i)*x/(2*i+1);
+        if (abs(SinMaclaurin(x,i))>0.001){
+            sin += SinMaclaurin(x,i);
         }
         else {
             break;
@@ -124,8 +103,6 @@ double mySqrt(double x) {
         m = (m + n) / 2;
         n = x / m;
     }
-    return x;
-    
-    return 0;
+    return m;
 }
 
