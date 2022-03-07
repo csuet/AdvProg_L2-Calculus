a1b75b37a3b36f9847088dec53c4318ce8084024
21020764-VuPhuongHong <100524815+21020764-VuPhuongHong@users.noreply.github.com>
2022-03-07 23:03:34 +0700

    21020764 commit

--- calculus.cpp
+++ calculus.cpp
@@ -12,6 +12,18 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
+double F(double x, int n) {
+    int a;
+    if (n % 2 == 0)
+        a = 1;
+    else
+        a = -1;
+    double temp = 1;
+    for (int i = 1; i <= 2*n+1; i++)
+        temp = temp * x / i;
+    return a * temp;
+}
+
 /***
     Args:
         x (double): a number
@@ -20,7 +32,13 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return cos(x);
+    double sin = 0;
+    int n = 0;
+    while (abs(F(x, n)) > 0.0000001) {
+        sin += F(x, n);
+        n++;
+    }
+    return sqrt(1-sin*sin);
 }
 
 /***
@@ -31,7 +49,13 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double sin = 0;
+    int n = 0;
+    while (abs(F(x, n)) > 0.0000001) {
+        sin += F(x, n);
+        n++;
+    }
+    return sin;
 }
 
 
@@ -46,7 +70,9 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return sqrt(x);
+    const double EPSILON = 0.0000001;
+    double result = 1.0;
+    while (abs(result * result - x) / x >= EPSILON)
+        result = (x / result  - result) / 2 + result;
+    return result;
 }
