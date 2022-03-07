7eea6fbe9c78d2a7c75ba105a15764260a019703
lnhi <nhiinguyen2406@gmail.com>
2022-03-07 22:41:42 +0700

    21020372-Nguyen Thi Lan Nhi

--- calculus.cpp
+++ calculus.cpp
@@ -7,27 +7,46 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
+double factorial( double number ) {
+    if ( number == 1 || number == 0 ) return 1;
+    else return number * factorial(number - 1);
+}
 
 double myCos(double x) 
 {
-    return cos(x);
+    double sum = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		sum += (pow(-1, i) * pow(x, 2 * i) / factorial(i * 2));
+	}
+	return sum;
 }
 
 double mySin(double x)
 {
-    return sin(x);
+    double sum = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		sum += (pow(-1, i) * pow(x, 2 * i + 1) / factorial(i * 2 + 1));
+	}
+	return sum;
 }
-
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
-        
     }
-    return sqrt(x) ;
+
+    double previousNumber = x;
+    double afterNumber = x;
+	for (int i = 0; i < 100; i++)
+	{
+		afterNumber = 1.0 / 2 * (previousNumber + x / previousNumber);
+		previousNumber = afterNumber;
+	}
+	return afterNumber;    
 }
