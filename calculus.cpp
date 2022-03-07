7d0dd550eeb2e73b656dead9ceacb67bb45b3e55
ngobinhduong <21020756@vnu.edu.vn>
2022-03-07 15:10:50 +0700

    21020756_NgoBinhDuong

--- calculus.cpp
+++ calculus.cpp
@@ -9,27 +9,39 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-double myCos(double x)
-{
-
-    return cos(x);
+double fac(int n){
+	if(n<2){
+		return 1;
+	}else{
+		return n*fac(n-1);
+	}
 }
 
-double mySin(double x)
-{
-
-    return sin(x);
+double myCos(double x){
+	double cosx = 0;
+	for(int i=0;i<100;i++){
+		cosx += pow(-1,i) * pow(x,i*2) * 1.0 / fac(i*2);
+	}
+	return cosx;
 }
 
+double mySin(double x){
+	double sinx = 0;
+	for(int j=0;j<100;j++){
+		sinx += pow(-1,j) * pow(x,j*2) * 1.0 / fac(j*2);
+	}
+	return sinx;
+}
 
 double mySqrt(double x) {
-	if(x<0){
-		cout << "Invalid argument" << endl;
-		exit(1);
-	}else{
-		return sqrt(x);
+    if (x < 0) {
+        cout << "Invalid argument" << endl;
+        exit(1);
+    }else{
+    	double sqrtx = 1.0;
+    	while(fabs(sqrtx * sqrtx - x) / x >= 0.0001){
+    		sqrtx = (x / sqrtx - sqrtx) / 2 + sqrtx;
+		}
+		return sqrtx;
 	}
-
-    return 0;
-
 }
