e4ac98944767418eb74056c0c9fbb3b9bcac81ca
uzi2991 <92511701+uzi2991@users.noreply.github.com>
2022-03-07 15:47:32 +0700

    Update calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -18,20 +18,35 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
-{
-    return cos(x);
+double mySin(double x) {
+    double cur = 1;
+    double res = 0;
+    for (int i = 1; cur >= 1e-6; ++i) {
+        cur *= x / i;
+        if (i % 4 == 1) {
+            res += cur;
+        } else if (i % 4 == 3) {
+            res -= cur;
+        }
+    }
+
+    return res;
 }
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
-double mySin(double x)
-{
-    return sin(x);
+double myCos(double x) {
+    double cur = 1;
+    double res = 1;
+    for (int i = 1; cur >= 1e-6; ++i) {
+        cur *= x / i;
+        if (i % 4 == 2) {
+            res -= cur;
+        } else if (i % 4 == 0) {
+            res += cur;
+        }
+    }
+
+    return res;
+
 }
 
 
@@ -47,5 +62,16 @@ double mySqrt(double x) {
         exit(1);
     }
 
-    return sqrt(x);
+        double l = 0, r = x;
+
+    while (r - l > 1e-6) {
+        double m = (l + r) / 2;
+        if (m * m > x) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+
+    return l;
 }
