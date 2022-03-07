c67714459fdb0b600ed25576bd6c74119b301844
cognaiger <21020369@vnu.edu.vn>
2022-03-07 22:00:42 +0700

    update 2nd calculus.cpp

--- calculus.cpp
+++ calculus.cpp
@@ -7,6 +7,13 @@ using std::cout;
 using std::endl;
 using std::stod;
 
+unsigned long long factorial(int num) {
+    unsigned long long res = 1;
+    for (int i = 1; i <= num; i++) {
+        res *= i;
+    }
+    return res;
+}
 
 double mySin(double x);
 double myCos(double x);
@@ -20,7 +27,11 @@ double mySqrt(double x);
 ***/
 double myCos(double x)
 {
-    return cos(x);
+    double res = 1;
+    for (int i = 1; i <= 15; i++) {
+        res += pow(-1, i)*pow(x, 2*i)/factorial(2*i);
+    }
+    return res;
 }
 
 /***
@@ -31,7 +42,11 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double res = 0;
+    for (int i = 0; i < 20; i++) {
+        res += pow(-1, i)*pow(x, 2*i + 1)/factorial(2*i + 1);
+    }
+    return res;
 }
 
 
@@ -42,9 +57,15 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
+    double res = 1;                          // initial guess
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
+    } else {
+        double epsilon = 1e-4;
+        for (int i = 0; i < 10; i++) {
+            res = (res + x/res)/2;
+        }
     }
-    return sqrt(x);
+    return res;
 }
