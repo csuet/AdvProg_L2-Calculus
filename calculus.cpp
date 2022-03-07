b17da812d68c7098791015e12335a23a381b1c6a
21021661hoangthanhtung <100946670+21021661hoangthanhtung@users.noreply.github.com>
2022-03-07 15:42:35 +0700

    21021661

--- calculus.cpp
+++ calculus.cpp
@@ -11,6 +11,7 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+#define esp 1e-5
 
 /***
     Args:
@@ -18,17 +19,15 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
     double cosx = 1;
     int n = 1;
     double temp = 1;
-    while (temp > 0.0001 || temp < -0,0001)
-    {
+    while (temp > 0.0001 || temp < -0.0001){
         temp = 1;
-        for(int i=1; i<=2*n; i++)
-        {
-            temp*= (x/(1));
+        for(int i=1; i<=2*n; i++){
+            temp*= (x/(i));
         }
     if(n % 2 == 0) cosx += temp;
     else cosx -= temp;
@@ -36,6 +35,7 @@ double myCos(double x)
     }
     return cosx;
 }
+
 
 /***
     Args:
@@ -45,21 +45,21 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    double sinx = 1;
+    double sinx = x;
     int n = 1;
     double temp = 1;
-    while (temp > 0.0001 || temp < -0,0001)
+    while (temp > 0.0001 || temp < -0.0001)
     {
        temp = 1;
         for(int i=1; i<=2*n+1; i++)
         {
-            temp*= (x/(1));
+            temp*= (x/(i));
         }
     if(n % 2 == 0) sinx += temp;
     else sinx -= temp;
     n++;
     }
-    
+    return sinx;
 }
 
 
@@ -69,12 +69,21 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
-double mySqrt(double x) {
-    if (x < 0) {
+using namespace std;
+double mySqrt(double x)
+{
+    if (x < 0)
+    {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return 0;
+    double b = x / 10;
+    double an=b;
+    double preAn = 0;
+    while(std::abs(an - preAn) > esp)
+    {
+        preAn = an;
+        an = 0.5 * (an + x / an);
+    }
+    return an;
 }
