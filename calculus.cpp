752e19e0125aa4d3d9c71f9afec6676c69ab1a86
Trần Quốc Huy <hn61078@gmail.com>
2022-03-07 17:31:36 +0700

    21020331_Tran Quoc Huy

--- calculus.cpp
+++ calculus.cpp
@@ -18,9 +18,22 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
+double gt(int n){
+    if(n==0) return 1;
+    return n*gt(n-1);
+}
 double myCos(double x) 
-{
-    return cos(x);
+{   
+    double k,kq=1;
+    int i=2;
+    do{ 
+        k = kq;
+        if(i%4==0) kq+=(1/gt(i))*double(pow(x,i));
+        else kq+=(-1/gt(i))*double(pow(x,i));
+        i+=2;
+    }while(abs(kq-k)>0.0001);
+    //cout<<kq;
+    return kq;
 }
 
 /***
@@ -31,7 +44,18 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double result = 0;
+    double k=1; int pos = 1;
+    while (abs((1/gt(k))*pow(x,k))>0.0001)
+    {
+        if (pos%2!=0)
+        {
+            result+=(1/gt(k))*pow(x,k);
+        }
+        else result-=(1/gt(k))*pow(x,k);
+        k+=2; pos++;
+    }
+    return result;
 }
 
 
@@ -41,12 +65,17 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
-double mySqrt(double x) {
-    if (x < 0) {
+double mySqrt(double number) {
+    if (number < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
 
     
-   else return sqrt(x);
+   else {
+    double result = 1.0f;
+    while (fabs(result * result - number) / number >= 0.0001)
+        result = (number / result  - result) / 2 + result;
+    return result;
+   }
 }
