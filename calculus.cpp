b115b87700071677c56e475ff94399e3a4942628
Ngô Duy <21020293@vnu.edu.vn>
2022-03-08 01:29:17 +0700

    Test

--- calculus.cpp
+++ calculus.cpp
@@ -27,13 +27,13 @@ double myCos(double x)
 {
     x= x * M_PI/180.0;
     int n=0;
-    double s=0,s1=0,s2=0;//s1 tong cac ptu tai vtri thu i, s2 tong cac ptu tai vtri i-1;
+    double s=0,s1=0,s2=0;
     do{
         s1=s2;
-        s+=(pow(-1,n)*pow(x,2*n))/(factorial(2*n));
+        s+=(pow((-1),n)*pow(x,2*n))/(factorial(2*n));
         s2=s;
         n++;
-    }while(abs(s2-s1)>0.00001);
+    }while(fabs(s2-s1)>0.00001);
     return s2;
 }
 
@@ -45,15 +45,15 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    x= x* M_PI/180.0;
+    x= x * M_PI/180;
     int n=0;
     double s=0,s1=0,s2=0;
     do{
         s1=s2;
-        s+=(pow(-1,n)*pow(x,2*n+1))/(factorial(2*n+1));
+        s+=(pow((-1),n)*pow(x,2*n+1))/(factorial(2*n+1));
         s2=s;
         n++;
-    }while(abs(s2-s1)>0.00001);
+    }while(fabs(s2-s1)>0.00001);
     return s2;
 }
 
@@ -69,17 +69,17 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
     }else{
-        while(fabs(ans * ans - x ) / x >= 0.000001){
+        while(fabs(ans * ans - x ) / x >= 0.00001){
             ans=( x / ans - ans)/ 2 + ans;
         }
     }
     return ans;
 }
-/*int main(){
+int main(){
     double x;
     cin>>x;
    // x= x * M_PI/180;
     cout<<myCos(x)<<endl;
     cout<<mySin(x)<<endl;
-    cout<<mySqrt(x)<<endl;
-}*/
+  //  cout<<mySqrt(x)<<endl;
+}
