6ac5f35b72b0c3839187bb3c52e3542bbdaa9f92
NguyenTienManh2409 <21020777@vnu.edu.vn>
2022-03-07 10:19:00 +0700

    Nguyen Tien Manh 21020777

--- calculus.cpp
+++ calculus.cpp
@@ -12,35 +12,22 @@ double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: cosine of x
-***/
+
 double myCos(double x) 
 {
-    return 0.0;
+    double myCos;
+    myCos=cos(x);
+    return myCos;
 }
 
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: sine of x
-***/
+
 double mySin(double x)
 {
-    return 0.0;
+    double mySin;
+    my=cos(x);
+    return myCos;
 }
 
-
-/***
-    Args:
-        x (double): a number
-    Returns:
-        double: square root of x
-***/
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
