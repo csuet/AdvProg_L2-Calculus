27bf64493aea1ba9a7ff411dc964ebbf90df3e5f
thanhedward <ngocthanh12082003d@gmail.com>
2022-03-07 20:11:32 +0700

    21020792 Trinh Duc Thanh

--- calculus.cpp
+++ calculus.cpp
@@ -17,9 +17,23 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return cos(x);
+	//return cos(x);
+
+	//Maclaurint series							
+	double m_prev=2, m=1, factorial=1, expo=1; 
+	int i=1;
+	const double Epsilon = 0.00001;
+	while(m-m_prev > Epsilon || m-m_prev < -Epsilon) //khi do chinh xac nho hon epsilon
+    {
+		m_prev=m; 								
+		factorial*=4*i*i-2*i; 					//tinh (2n)!
+		expo*=x*x;								//tinh x^(2n)
+		m+=(i%2==0?1:-1)*expo/factorial;	    
+		i++;
+	}
+	return m;
 }
 
 /***
@@ -30,7 +44,21 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    //return sin(x);
+
+	//Maclaurint series
+	double m_prev=x-1, m=x, factorial=1, expo=x;
+	int i=1;
+	const double Epsilon = 0.00001;
+	while(m-m_prev > Epsilon || m-m_prev < -Epsilon)
+    {
+		m_prev=m;
+		factorial*=4*i*i+2*i;
+		expo*=x*x;
+		m+=(i%2==0?1:-1)*expo/factorial;
+		i++;
+	}
+	return m;
 }
 
 
@@ -45,6 +73,15 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-    
-    return sqrt(x);
+	//return sqrt(x);
+
+	//Babylonian method
+	double b_prev=x, b=1; 	
+	const double Epsilon = 0.00001;
+	while(b-b_prev > Epsilon || b-b_prev < -Epsilon)
+    {
+		b_prev = b;
+		b = (b + x/b) /2;
+	}
+    return b;
 }
