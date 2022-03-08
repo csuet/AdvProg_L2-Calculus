c5ec73ad0621cfb71fdeb3daac0a511903e1e617
guenfon <92385828+guenfon@users.noreply.github.com>
2022-03-08 10:42:20 +0700

    Nguyen Hai Phong-21020085

--- calculus.cpp
+++ calculus.cpp
@@ -23,7 +23,7 @@ double myCos(double x)
     double cosX = 1;
     double factorial = 1;
     double oldcosX = 0;
-    int n = 0;
+    double n = 0;
     do
     {
         ++n;
@@ -31,7 +31,7 @@ double myCos(double x)
         ++n;
         oldcosX = cosX;
         cosX = cosX - 1.0/factorial * pow(x, n) * pow(-1, n/2 + 1);
-    } while (abs(cosX - oldcosX) > 0.000001);
+    } while (abs(cosX - oldcosX) > 0.001);
     return cosX;
 }
 
@@ -47,7 +47,7 @@ double mySin(double x)
     double sinX = x;
     double factorial = 1;
     double oldsinX = 0;
-    int n = 1;
+    double n = 1;
     do
     {
         ++n;
@@ -55,7 +55,7 @@ double mySin(double x)
         ++n;
         oldsinX = sinX;
         sinX = sinX - 1.0/factorial * pow(x, n) * pow(-1, (n-1)/2 + 1);
-    } while (abs(sinX - oldsinX) > 0.01);
+    } while (abs(sinX - oldsinX) > 0.001);
     return sinX;
 }
 
@@ -72,7 +72,7 @@ double mySqrt(double x) {
         exit(1);
     }
     double result = 1;
-    while (fabs(result * result - x) / x >= 0.01)
+    while (fabs(result * result - x) / x >= 0.000001)
         result = (x / result  - result) / 2 + result;
     return result;
 }
