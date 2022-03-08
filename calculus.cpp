8c5cc59ae9895dbd20c153880819d5671536f22d
duykhanhxx03 <duykhanhxx03@gmail.com>
2022-03-08 09:54:52 +0700

    OK

--- calculus.cpp
+++ calculus.cpp
@@ -7,7 +7,10 @@ using std::cout;
 using std::endl;
 using std::stod;
 
-
+double factorial(double x){
+    if (x==0||x==1) return 1;
+    return x*factorial(x-1);
+}
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
@@ -18,9 +21,14 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
-{
-    return cos(x);
+double myCos(double x){
+    double sum=1; double oldsum=0;
+    int i=0;
+    while(fabs(sum-oldsum)>0.00001){
+        oldsum=sum; i++;
+        sum += (pow(-1, i) * pow(x, 2 * i) / factorial(i * 2));
+    }
+    return sum;
 }
 
 /***
@@ -29,9 +37,14 @@ double myCos(double x)
     Returns:
         double: sine of x
 ***/
-double mySin(double x)
-{
-    return sin(x);
+double mySin(double x){
+    double sum=x; double oldsum=0;
+    int i=0;
+    while(fabs(sum-oldsum)>0.00001){
+        oldsum=sum; i++;
+        sum+=(pow(-1,i)*(pow(x,2*i+1))/factorial(2*i+1));
+    }
+    return sum;
 }
 
 
@@ -41,12 +54,14 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
+#define EPSILON 0.0001f
 double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return sqrt(x);
+    double result = 1.0f;
+    while (fabs(result * result - x) / x >= EPSILON)
+        result = (x / result  - result) / 2 + result;
+    return result;
 }
