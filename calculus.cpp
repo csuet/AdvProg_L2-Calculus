4152f6875a97df578cc41e318c57f9e52195dc1a
Nguyen Tien Hai <sadtearcheslea@gmail.com>
2022-03-08 11:42:42 +0700

    hello

--- calculus.cpp
+++ calculus.cpp
@@ -2,11 +2,10 @@
 #include <string>
 #include <cmath>
 
-using std::string;
 using std::cout;
 using std::endl;
 using std::stod;
-
+using std::string;
 
 double mySin(double x);
 double myCos(double x);
@@ -18,9 +17,27 @@ double mySqrt(double x);
     Returns:
         double: cosine of x
 ***/
-double myCos(double x) 
+double myCos(double x)
 {
-    return 0.0;
+    while (x > 2 * M_PI)
+        x -= 2 * M_PI;
+    double cos = 1.00;
+    int n = 1;
+    double temp = 1.00;
+    while (temp > 0.00001)
+    {
+        temp = 1.00;
+        for (int i = 1; i <= 2 * n; i++)
+        {
+            temp *= x * 1.00 / i;
+        }
+        if (n % 2 == 0)
+            cos += temp;
+        else
+            cos -= temp;
+        n++;
+    }
+    return cos;
 }
 
 /***
@@ -31,9 +48,26 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    while (x > 2 * M_PI)
+        x -= 2 * M_PI;
+    double sin = x * 1.00;
+    int n = 1;
+    double temp = 1.00;
+    while (temp > 0.00001)
+    {
+        temp = 1.00;
+        for (int i = 1; i <= 2 * n; i++)
+        {
+            temp *= x * 1.00 / i;
+        }
+        if (n % 2 == 0)
+            sin += temp;
+        else
+            sin -= temp;
+        n++;
+    }
+    return sin;
 }
-
 
 /***
     Args:
@@ -41,12 +75,15 @@ double mySin(double x)
     Returns:
         double: square root of x
 ***/
-double mySqrt(double x) {
-    if (x < 0) {
+double mySqrt(double x)
+{
+    if (x < 0)
+    {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
-    return 0;
+    double result = 1.0f;
+    while (fabs(result * result - number) / number >= EPSILON)
+        result = (number / result  - result) / 2 + result;
+    return result;
 }
