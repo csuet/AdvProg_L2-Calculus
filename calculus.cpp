b0d6784ab85ba0f0393d88d1528a0ecd6864ed8b
unknown <21020538@vnu.edu.vn>
2022-03-08 13:43:30 +0700

    Improved Calculus

--- calculus.cpp
+++ calculus.cpp
@@ -11,6 +11,17 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+double factorial(double x);
+
+double factorial(double x)
+{
+    double factorial = 1;
+    for(int i = 1; i <= x; i ++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
 
 /***
     Args:
@@ -20,7 +31,21 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return cos(x);
+    double newX = x;
+    while(newX > 2 * M_PI)
+    {
+        newX -= 2 * M_PI;
+    }
+    double n = 1;
+    double myCos = 1; 
+    long double tmp = 1;
+    while(tmp < -0.0001 || tmp > 0.0001)
+    {
+        tmp = pow(-1, n) * (pow(newX, 2 * n) / factorial(2 * n));
+        myCos += tmp;
+        n ++;
+    }
+    return myCos;
 }
 
 /***
@@ -31,7 +56,21 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    double newX = x;
+    while(newX > 2 * M_PI)
+    {
+        newX -= 2 * M_PI;
+    }
+    double n = 1;
+    double mySin = newX; 
+    long double tmp = 1;
+     while(tmp < -0.0001 || tmp > 0.0001)
+    {
+        tmp = pow(-1, n) * (pow(newX, (2 * n + 1)) / factorial(2 * n + 1));
+        mySin += tmp;
+        n ++;
+    }
+    return mySin;
 }
 
 
@@ -46,7 +85,14 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    double mySqrt = 1;
+    double myOldSqrt = 0; 
+    while(abs(mySqrt - myOldSqrt) > 0.0001)
+    {
+        myOldSqrt = mySqrt;
+        mySqrt = mySqrt - ((pow(mySqrt, 2) - x) / (2 * mySqrt));
+    }
+    return mySqrt; 
     
     return sqrt(x);
 }
