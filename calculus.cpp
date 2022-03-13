ca5d9a3c8d5bbf0621e5c604ebd784a1cb8d2cb3
hoang nguyen <nguyenthehoang98@gmail.com>
2022-03-13 17:02:58 +0700

    Update calculus

--- calculus.cpp
+++ calculus.cpp
@@ -11,16 +11,44 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
-
+long long factorial(long n);
 /***
     Args:
         x (double): a number
     Returns:
         double: cosine of x
 ***/
+
+double factorial(int n)
+{
+    double ans = 1;
+    for(int i=1; i<=n; i++){
+        ans *= i;
+    }
+    return ans;
+}
+
 double myCos(double x) 
 {
-    return cos(x);
+	while (x > 2*PI_TEST){
+		x -= 2*PI_TEST;
+	}
+	while (x < -2*PI_TEST){
+		x += 2*PI_TEST;
+	}
+    double cos = 1;
+    int i = 1;
+    double temp = 1;
+    while(temp > 0.00000000001){
+        temp = pow(x,2*i)/factorial(2*i);
+        if (i % 2 == 0){
+            cos += temp;
+        }else{
+            cos -= temp;
+        }
+        i++;
+    }
+    return cos;
 }
 
 /***
@@ -31,7 +59,25 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return sin(x);
+    while (x > 2*PI_TEST){
+		x -= 2*PI_TEST;
+	}
+	while (x < -2*PI_TEST){
+		x += 2*PI_TEST;
+	}
+    double sin = 0;
+    int i = 0;
+    double temp = 1;
+    while(temp > 0.0000000000001){
+        temp = pow(x,2*i+1) / factorial(2*i+1);
+        if (i % 2 == 0){
+            sin += temp;
+        }else{
+            sin -= temp;
+        }
+        i++;
+    }
+    return sin;
 }
 
 
@@ -46,6 +92,13 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }else{
-        return sqrt(x);
+        double sqrt[100];
+		sqrt[0] = 1.00;
+		int i = 1;
+		while (sqrt[i-1] - sqrt[i] > 0.000001){
+			sqrt[i] = 0.5 * (sqrt[i-1] + x/sqrt[i-1]);
+			i++;
+		}
+		return sqrt[i-1];
     }
 }
