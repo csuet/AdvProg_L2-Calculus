1cca4db3aa2c033ae4a4af81ab547231b3c8e944
Thach21020789 <21020789@vnu.edu.vn>
2022-03-14 21:18:13 +0700

    update

--- calculus.cpp
+++ calculus.cpp
@@ -20,7 +20,7 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    return cos(x);
 }
 
 /***
@@ -31,9 +31,8 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    return sin(x);
 }
-
 
 /***
     Args:
@@ -46,7 +45,5 @@ double mySqrt(double x) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
-    
     return 0;
 }
