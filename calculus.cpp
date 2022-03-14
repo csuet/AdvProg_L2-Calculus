5886efd8c83d5f863f28b470bc8891fe1a6c2e00
tthanh25 <21020790@vnu.edu.vn>
2022-03-14 21:33:22 +0700

    lakgjla

--- calculus.cpp
+++ calculus.cpp
@@ -11,6 +11,10 @@ using std::stod;
 double mySin(double x);
 double myCos(double x);
 double mySqrt(double x);
+double factorial(double x){
+    if (x==0||x==1) return 1;
+    return x*factorial(x-1);
+}
 
 /***
     Args:
@@ -20,7 +24,13 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+     double sum=1; double oldsum=0;
+    int i=0;
+    while(fabs(sum-oldsum)>0.00001){
+        oldsum=sum; i++;
+        sum += (pow(-1, i) * pow(x, 2 * i) / factorial(i * 2));
+    }
+    return sum;
 }
 
 /***
@@ -31,7 +41,13 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+   double sum=x; double oldsum=0;
+    int i=0;
+    while(fabs(sum-oldsum)>0.00001){
+        oldsum=sum; i++;
+        sum+=(pow(-1,i)*(pow(x,2*i+1))/factorial(2*i+1));
+    }
+    return sum;
 }
 
 
@@ -42,11 +58,14 @@ double mySin(double x)
         double: square root of x
 ***/
 double mySqrt(double x) {
-    if (x < 0) {
+   if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
     }
-
+    double result = 1.0;
+    while (fabs(result * result - x) / x >= 0.0001)
+        result = (x / result  - result) / 2 + result;
+    return result;
     
     return 0;
 }
